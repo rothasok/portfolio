e21a60cb82e2a94cac74c5cb5cef5bad8feb2ce8
rotha sok <rothasok@gmail.com>
2024-10-05 18:25:31 +0700

    not found

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,17 +7,7 @@ import Portfolio from "@/views/Portfolio.vue"
 import Blog from "@/views/Blog.vue"
 import Contact from "@/views/Contact.vue"
 import Post from '@/components/Post.vue';
-// import About from "@/pages/About.vue"
-// import Team from "@/pages/Team/Team.vue"
-// import TeamComponent from "@/components/Team.vue"
-// import TeamMember from "@/pages/Team/TeamMember.vue"
-// import Services from "@/components/Services.vue"
-// import Course from "@/pages/courses/index.vue"
-// import CoursePage from '@/pages/courses/CoursePage.vue'
-// import Login from "@/pages/login.vue"
-// import Repository from '@/pages/Repository.vue'
-// import Register from '@/pages/Register.vue'
-// import NotFound from "@/components/NotFound.vue"
+import NotFound from "@/components/NotFound.vue"
 const routes = [
     { path: '/', component: Home },
     { path: '/about', component: About },
@@ -30,22 +20,8 @@ const routes = [
         name: 'post',
         component: Post,
       },
+      {path: '/:pathMatch(.*)*', component: NotFound}
 ];
-
-    // { path: '/about', component: About },
-    // {
-    //     path: '/team', component: Team, children: [
-    //         { path: ':id', component: TeamMember },
-    //         { path: '', component: TeamComponent },
-    //         { path: 'service', component: Services }
-    //     ]
-    // },
-    // { path: '/courses', component: Course },
-    // { path: '/courses/:id', component: CoursePage },
-    // { path: '/login', component: Login },
-    // { path: '/repos', component: Repository },
-    // { path: '/register', component: Register },
-    // {path: '/:pathMatch(.*)*', component: NotFound}
 
 
 const router = createRouter({
